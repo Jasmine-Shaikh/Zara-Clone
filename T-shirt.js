609226a0c24f79215a7a1f50c043fb69c0277bd1
Manjusha003 <102724843+Manjusha003@users.noreply.github.com>
2022-04-13 19:39:19 +0530

    woman page

---
+++ T-shirt.js
@@ -0,0 +1,167 @@
+        var TshirtsData = [
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/2335/159/941/2/w/169/2335159941_2_2_1.jpg?ts=1649342414634",
+
+                type: "Cropped T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2021/V/0/1/p/4424/159/800/2/w/392/4424159800_6_1_1.jpg?ts=1611826835336",
+                type: "Cropped T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2021/I/0/1/p/4424/959/250/2/w/392/4424959250_6_1_1.jpg?ts=1635870678626",
+                type: "Cropped T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2021/I/0/1/p/4424/159/710/2/w/392/4424159710_6_1_1.jpg?ts=1623747453873",
+                type: "Cropped T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/4424/959/400/2/w/392/4424959400_6_1_1.jpg?ts=1647509002867",
+                type: "Cropped T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2021/I/0/1/p/4424/959/600/2/w/392/4424959600_6_1_1.jpg?ts=1626770913505",
+                type: "Cropped T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/2335/159/628/2/w/392/2335159628_6_1_1.jpg?ts=1643814558901",
+                type: "Cropped T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/2335/159/539/2/w/392/2335159539_6_1_1.jpg?ts=1643814566919",
+                type: "Cropped T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/1131/321/250/2/w/392/1131321250_2_1_1.jpg?ts=1649412966935",
+                type: "Girl's printed T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/5039/325/250/2/w/392/5039325250_1_1_1.jpg?ts=1649345518291",
+                type: "Printed Ny T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/5644/313/060/2/w/392/5644313060_2_1_1.jpg?ts=1649345535367",
+                type: "Printed T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/1131/320/250/2/w/392/1131320250_1_1_1.jpg?ts=1649242622882",
+                type: "Girl's printed T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/1131/316/250/2/w/392/1131316250_1_1_1.jpg?ts=1649175495220",
+                type: "Printed T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/1131/317/807/2/w/392/1131317807_2_1_1.jpg?ts=1649175481336",
+                type: "Faded-effect Printed T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/0264/370/400/2/w/392/0264370400_1_1_1.jpg?ts=1649175487762",
+                type: "Printed T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/0264/370/063/2/w/392/0264370063_6_1_1.jpg?ts=1649070095888",
+                type: "Printed T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/0264/370/300/2/w/312/0264370300_6_1_1.jpg?ts=1646328032414",
+                type: "Printed T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/0264/370/064/2/w/312/0264370064_6_1_1.jpg?ts=1646208458309",
+                type: "Printed T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/0264/370/620/2/w/312/0264370620_6_1_1.jpg?ts=1646208477103",
+                type: "Printed T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/0264/370/500/2/w/312/0264370500_6_1_1.jpg?ts=1646208452980",
+                type: "Cropped Tshirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/0962/349/620/2/w/312/0962349620_2_1_1.jpg?ts=1646049201940",
+                type: "Basic T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/0962/349/800/2/w/312/0962349800_2_2_1.jpg?ts=1646049200824",
+                type: "Basic T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/0962/349/620/2/w/312/0962349620_15_10_1.jpg?ts=1645792134094",
+                type: "Basic T-shirt",
+                price: "590.00",
+            },
+            {
+                image_url:
+                    "https://static.zara.net/photos///2022/V/0/1/p/0962/349/800/2/w/312/0962349800_15_10_1.jpg?ts=1645792133289",
+                type: "Basic T-shirt",
+                price: "590.00",
+            },
+        ];
+
+        TshirtsData.forEach(function (Tshirt) {
+            var box = document.createElement("div");
+            box.setAttribute("class", "box");
+
+            var img = document.createElement("img");
+            img.src = Tshirt.image_url;
+
+
+
+            var type = document.createElement("div");
+            type.textContent = Tshirt.type;
+            type.style.color = "rgb(143, 143, 140)";
+            var price = document.createElement("div");
+            price.innerText = "₹" + Tshirt.price;
+
+            box.append(img, type, price);
+
+            document.querySelector("#container").append(box);
+        });
