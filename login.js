848caca24760281651c9fb8e9ab0e09249b86e5e
Jasmine Shaikh <jasmineshaikh54@gmail.com>
2022-04-18 15:54:43 +0530

    Rectifications in signup and login page

--- login.js
+++ login.js
@@ -1,28 +1,46 @@
 var userData = JSON.parse(localStorage.getItem("userDataBase"));
 
 function clickForPass() {
-  var height = (document.querySelector("#inputEmail").style.height = "block");
-  var passwd = (document.querySelector("#inputPasswd").style.display = "block");
+    var height = (document.querySelector("#inputEmail").style.height = "block");
+    var passwd = (document.querySelector("#inputPasswd").style.display = "block");
 }
 
 document.querySelector("#form").addEventListener("submit", logInBtn);
 
 function logInBtn(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  var email = document.querySelector("#inputEmail").value;
-  var password = document.querySelector("#inputPasswd").value;
-  var count = 0;
+    var email = document.querySelector("#inputEmail").value;
+    var password = document.querySelector("#inputPasswd").value;
+    var count = 0;
 
-  for (var i = 0; i < userData.length; i++) {
-    if (userData[i].email == email && userData[i].password == password) {
-      count = 1;
+    for (var i = 0; i < userData.length; i++) {
+        if (userData[i].email == email && userData[i].password == password) {
+            count = 1;
+        }
     }
-  }
-  if (count == 1) {
-    alert("Login Successful");
-    window.location.href = "homePage.html";
-  } else {
-    alert("Invalid Credentials");
-  }
+    if (count == 1) {
+        alert("Login Successful");
+        window.location.href = "homePage.html";
+    } else {
+        alert("Invalid Credentials");
+    }
 }
+
+// -------------------------Category Modal----------------------
+
+var menuModal = document.getElementById('menuModal');
+
+document.querySelector('.close').addEventListener('click', function() {
+    menuModal.classList.add('animatedMenuShow');
+
+})
+
+menuModal.addEventListener('animationend', function() {
+    if (this.classList.contains('animatedMenuShow')) {
+        this.style.display = 'none';
+        this.classList.remove('animatedMenuShow')
+    }
+
+
+});
--- signup.js
+++ signup.js
@@ -8,7 +8,7 @@ function signUp(event) {
     var reapeatpassword = document.querySelector("#reapeatpassword").value;
     var name = document.querySelector("#name").value;
     var pin = document.querySelector("#pin").value;
-   
+
     var address = document.querySelector("#address").value;
     var optional = document.querySelector("#optional").value;
     var locality = document.querySelector("#locality").value;
@@ -19,37 +19,56 @@ function signUp(event) {
     var tel = document.querySelector("#tel").value;
     var checkBox = document.getElementById("checkmark").checked
     var checkBox1 = document.getElementById("checkmark1").checked;
-    if (name == "" || pin == "" || email == "" || password == "" || reapeatpassword == "" || address== "" || optional == "" ||
-     locality == "" || city == "" || state == "" || stateR == "" || mobile == "" || tel == "" || checkBox == false ||checkBox1 == false) {
+    if (name == "" || pin == "" || email == "" || password == "" || reapeatpassword == "" || address == "" ||
+        locality == "" || city == "" || state == "" || stateR == "" || mobile == "" || tel == "" || checkBox == false || checkBox1 == false) {
         alert("Please enter all data")
     } else {
         var obj = {
-            name: name,
-            pin: pin,
-            email: email,
-           
-            password: password,
-            reapeatpassword:reapeatpassword,
-            address:address,
-            optional:optional,
-            locality :locality ,
-            city: city,
-            state:state,
-            stateR: stateR,
-            tel:tel,
+                name: name,
+                pin: pin,
+                email: email,
+
+                password: password,
+                reapeatpassword: reapeatpassword,
+                address: address,
+                optional: optional,
+                locality: locality,
+                city: city,
+                state: state,
+                stateR: stateR,
+                tel: tel,
 
 
 
 
 
 
-        }
-        // console.log(obj)
+            }
+            // console.log(obj)
         usersData.push(obj);
-        
+
         window.location.href = "login.html";
     }
 
     localStorage.setItem("userDataBase", JSON.stringify(usersData))
 
 }
+
+
+// ---------------------Category Modal-----------------------------
+
+var menuModal = document.getElementById('menuModal');
+
+document.querySelector('.close').addEventListener('click', function() {
+    menuModal.classList.add('animatedMenuShow');
+
+})
+
+menuModal.addEventListener('animationend', function() {
+    if (this.classList.contains('animatedMenuShow')) {
+        this.style.display = 'none';
+        this.classList.remove('animatedMenuShow')
+    }
+
+
+});
